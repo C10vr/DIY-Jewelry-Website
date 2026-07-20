ad573d99ab10da4e86f593ee87b08383507342be
ghdwlgp <tienle.lee@gmail.com>
2026-07-20 20:31:23 +0800

    Cache

--- DIY Jewelry Website/Home.aspx.cs
+++ DIY Jewelry Website/Home.aspx.cs
@@ -11,6 +11,13 @@ namespace DIY_Jewelry_Website
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            // Prevent Cache 
+
+            Response.Cache.SetCacheability(HttpCacheability.NoCache);
+            Response.Cache.SetNoStore();
+            Response.Cache.SetExpires(DateTime.UtcNow.AddDays(-1));
+            Response.Cache.SetRevalidation(HttpCacheRevalidation.AllCaches);
+            Response.Cache.SetValidUntilExpires(false);
 
         }
     }
