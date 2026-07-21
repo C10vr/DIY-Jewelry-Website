cc9f9bd64e3c9b9cbe5a5b18e85e7e842de2f67b
hazeeqffs <hazeeqamzarhaslee@gmail.com>
2026-07-22 00:58:16 +0800

    remove the database and create it locally

---
+++ DIY Jewelry Website/Database/DIYJewelryDB.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE userTable
+(
+    Id INT IDENTITY(1,1) PRIMARY KEY,
+    FullName NVARCHAR(100) NOT NULL,
+    EmailAddress NVARCHAR(100) NOT NULL,
+    Username NVARCHAR(50) NOT NULL,
+    Password NVARCHAR(100) NOT NULL,
+    UserType INT NOT NULL DEFAULT 1
+);
+
+INSERT INTO userTable
+(FullName, EmailAddress, Username, Password, UserType)
+VALUES
+('Admin', 'admin@diy.com', 'Admin', 'Admin123', 2);
